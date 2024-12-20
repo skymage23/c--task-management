860a571dc2f0c2ad94da5ff3a65785c12ae31fea
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2024-12-20 16:03:58 -0600

    Created the "Letter" class. Created the skeleton of the "Mailbox" base class.

---
+++ include/messaging/letter.hpp
@@ -0,0 +1,11 @@
+#ifndef CPLUSPLUS_TASK_MANAGEMENT_INCLUDE_LETTER_HPP
+#define CPLUSPLUS_TASK_MANAGEMENT_INCLUDE_LETTER_HPP
+namespace task_mgnt {
+    namespace messaging {
+        class Letter{
+
+        };
+    };
+};
+
+#endif
--- include/messaging/mailbox.hpp
+++ include/messaging/mailbox.hpp
@@ -1,14 +1,37 @@
 #ifndef CPLUSPLUS_TASK_MANAGEMENT_INCLUDE_MAILBOX_HPP
 #define CPLUSPLUS_TASK_MANAGEMENT_INCLUDE_MAILBOX_HPP
+
+#include <memory>
+#include <vector>
+
+#include <letter.hpp>
+
 namespace task_mgnt {
     namespace messaging {
         class Mailbox {
-            public:
-            
+            private:
+            Mailbox* other_party;
+            bool have_mail;
+            bool can_send; //Not all Mailboxes can.
+ 
+            protected:
+            //Used when instantiating other parties.
+            Mailbox(Mailbox* input); 
+
             public:
             Mailbox() = delete;
             Mailbox(Mailbox& input) = delete;
             Mailbox(Mailbox&& input) = delete;
+            
+
+            virtual Mailbox* create_other_party();
+            //Notify interested parties (if any) that there
+            //is new mail. 
+            virtual void notify_recipient();
+            virtual void send(Letter& letter);
+            virtual Letter receive();
+            bool mail_for_me();
+            bool can_send();
         };
     };
 };
