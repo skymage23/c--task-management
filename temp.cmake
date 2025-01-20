66a1bb8baf2a27f26ee96ecd84595ee0f4ea8b4b
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-01-19 21:30:01 -0600

    Added macOS support to os_host_setup.cmake. Changed temp.cmake to be more OS-agnostic.

--- temp.cmake
+++ temp.cmake
@@ -2,6 +2,6 @@ cmake_minimum_required(VERSION 3.10)
 include(os_host_setup.cmake)
 
 #look_for_dependency_program()
-get_realpath("/home/cnugent/Documents")
+get_realpath("$ENV{HOME}/Documents")
 message(STATUS "REALPATH: ${REALPATH}")
 #message(STATUS "PRG: ${PRG}")
