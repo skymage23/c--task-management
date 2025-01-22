1f7bb83b02f68e93ca5a6cc9bbce4a975cf100c4
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-01-22 10:07:18 -0600

    Updated "/scripts/scripts_tests/cmake/test_helpers/test_get_dirs_with_cmake_lists_timeout/CMakeLists.txt" t0 support CMake 3.30 and up. Updated "/scripts/scripts_tests/test_helpers/test_get_dirs_with_cmake_lists.txt" to invoke "/scripts/scripts_tests/cmake/test_helpers/test_get_dirs_with_cmake_lists_timeout/CMakeLists.txt" when running CMake in the forked process.

--- scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
+++ scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/common.cmake)
 include(common.cmake)
 
-execute_process( COMMAND ${CMAKE_COMMAND}  ${TEST_HELPERS_DIR}/timeout_test_get_dirs_with_cmake_lists/CMakeLists.txt
+execute_process( COMMAND ${CMAKE_COMMAND}  ${TEST_HELPERS_DIR}/test_get_dirs_with_cmake_lists_timeout/CMakeLists.txt
     TIMEOUT 1s
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/build
     COMMAND_ECHO STDOUT
