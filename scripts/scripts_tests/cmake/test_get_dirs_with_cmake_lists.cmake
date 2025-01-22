0b30666b66a87c219d5eafdec1b2e62dccad3ecb
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-01-22 10:15:01 -0600

    Updated "/scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake" so that it creates the "build" directory and destroys it when the test completes successfully.

--- scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
+++ scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
@@ -1,9 +1,18 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/common.cmake)
 include(common.cmake)
 
+set(BUILD_DIR_PATH "${CMAKE_CURRENT_LIST_DIR}/build")
+
+if(NOT EXISTS "${BUILD_DIR_PATH}")
+    make_directory("${BUILD_DIR_PATH}")
+endif()
+
 execute_process( COMMAND ${CMAKE_COMMAND}  ${TEST_HELPERS_DIR}/test_get_dirs_with_cmake_lists_timeout/CMakeLists.txt
     TIMEOUT 1s
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/build
     COMMAND_ECHO STDOUT
     COMMAND_ERROR_IS_FATAL ANY
 )
+
+file(REMOVE_RECURSE "${BUILD_DIR_PATH}")
+
