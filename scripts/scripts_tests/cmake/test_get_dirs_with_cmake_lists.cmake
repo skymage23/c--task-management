33b57809a41e796d5602a8473c3e222190885155
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-01-22 08:23:16 -0800

    Updated /scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake to have a more lenient timeout. Windows build file generation for this test takes about 5 seconds, and was causing the test script to timeout before it could properly be run.

--- scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
+++ scripts/scripts_tests/cmake/test_get_dirs_with_cmake_lists.cmake
@@ -8,7 +8,7 @@ if(NOT EXISTS "${BUILD_DIR_PATH}")
 endif()
 
 execute_process( COMMAND ${CMAKE_COMMAND}  ${TEST_HELPERS_DIR}/test_get_dirs_with_cmake_lists_timeout/CMakeLists.txt
-    TIMEOUT 1s
+    TIMEOUT 4s
     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/build
     COMMAND_ECHO STDOUT
     COMMAND_ERROR_IS_FATAL ANY
